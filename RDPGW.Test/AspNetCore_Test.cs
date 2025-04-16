7c7baa17e7461d2c5190b09c416304bb6ab56811
Maximilian Kenfenheuer <maximilian.kenfenheuer@ksol.it>
2025-04-16 10:49:36 +0200

    Additional auth Tests
    Signed-off-by: Maximilian Kenfenheuer <maximilian.kenfenheuer@ksol.it>

--- RDPGW.Test/AspNetCore_Test.cs
+++ RDPGW.Test/AspNetCore_Test.cs
@@ -18,7 +18,7 @@ public class AspNetCore_Test
 
         builder.WebHost.UseKestrel(options =>
         {
-            options.ListenLocalhost(50001);
+            options.ListenLocalhost(50000);
         });
 
         // Add services to the container.
@@ -28,7 +28,7 @@ public class AspNetCore_Test
         builder.Services.AddRDPGW();
 
         builder.Services.AddSingleton<IRDPGWAuthenticationHandler, AuthHandler>();
-        builder.Services.AddSingleton<IRDPGWAuthorizationHandler, AuthorizationHandler>();
+        builder.Services.AddSingleton<IRDPGWAuthorizationHandler, SuccessAuthorizationHandler>();
 
         var app = builder.Build();
 
@@ -44,7 +44,7 @@ public class AspNetCore_Test
         await Task.Delay(1000);
 
         var client = new HttpClient();
-        var response = await client.GetAsync("http://localhost:50001/");
+        var response = await client.GetAsync("http://localhost:50000/");
 
         Assert.IsFalse(response.IsSuccessStatusCode, "Response was successful. Expected failure.");
         Assert.AreEqual(404, (int)response.StatusCode, "Expected 404 Not Found status code.");
@@ -64,7 +64,7 @@ public class AspNetCore_Test
     }
 
     [TestMethod]
-    public async Task TestWebServer()
+    public async Task TestWebServerNegotiateAuth()
     {
         var builder = WebApplication.CreateBuilder([]); ;
 
@@ -80,7 +80,84 @@ public class AspNetCore_Test
         builder.Services.AddRDPGW();
 
         builder.Services.AddSingleton<IRDPGWAuthenticationHandler, AuthHandler>();
-        builder.Services.AddSingleton<IRDPGWAuthorizationHandler, AuthorizationHandler>();
+        builder.Services.AddSingleton<IRDPGWAuthorizationHandler, SuccessAuthorizationHandler>();
+
+        var app = builder.Build();
+
+        app.UseRDPGW();
+
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        var appTask = Task.Run(app.Run);
+
+        await Task.Delay(1000);
+
+        var client = new HttpClient();
+        var response = await client.GetAsync("http://localhost:50000/");
+
+        Assert.IsFalse(response.IsSuccessStatusCode, "Response was successful. Expected failure.");
+        Assert.AreEqual(404, (int)response.StatusCode, "Expected 404 Not Found status code.");
+
+        ClientWebSocket ws = new ClientWebSocket();
+        try
+        {
+            ws.Options.SetRequestHeader("Authorization", "Negotiate " + Convert.ToBase64String(Encoding.UTF8.GetBytes("user:password")));
+            await ws.ConnectAsync(new Uri("ws://localhost:50000/remoteDesktopGateway"), CancellationToken.None);
+        }
+        catch 
+        {
+            Assert.Fail("Expected WebSocket to be accepted.");
+            return;
+        }
+
+        Assert.IsTrue(ws.State == WebSocketState.Open, "WebSocket is not open.");
+
+        var packets = JsonConvert.DeserializeObject<TestPacket[]>(File.ReadAllText("packets.json"));
+        Assert.IsNotNull(packets, "Failed to deserialize packets.json");
+
+
+        var clientPackets = packets!.Where(p => p.Type == "client" && p.TypeName != "HTTP_DATA_PACKET").ToArray();
+
+        foreach(var packet in clientPackets)
+        {
+            Console.WriteLine($"Sending packet: {packet.TypeName}");
+            await ws.SendAsync(packet.Data, WebSocketMessageType.Binary, true, CancellationToken.None);
+            var buffer = new byte[10240];
+            Console.WriteLine($"Waiting for packet: {packet.Expected}");
+            var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            Assert.IsTrue(result.MessageType == WebSocketMessageType.Binary, "Expected binary message type.");
+            Assert.IsTrue(result.EndOfMessage, "Expected end of message.");
+            var data = buffer.Take(result.Count).ToArray();
+        
+            var packetMessage = HTTP_PACKET.FromBytes(data);
+            Assert.IsTrue(packetMessage.GetType().Name == packet.Expected, $"Type mismatch for packet: {packetMessage.GetType().Name} != {packet.Expected}");
+        }
+
+        await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+        Assert.IsTrue(ws.State == WebSocketState.Closed, "WebSocket is not closed.");
+    }
+
+    [TestMethod]
+    public async Task TestWebServerBasicAuth()
+    {
+        var builder = WebApplication.CreateBuilder([]); ;
+
+        builder.WebHost.UseKestrel(options =>
+        {
+            options.ListenLocalhost(50000);
+        });
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddRDPGW();
+
+        builder.Services.AddSingleton<IRDPGWAuthenticationHandler, AuthHandler>();
+        builder.Services.AddSingleton<IRDPGWAuthorizationHandler, SuccessAuthorizationHandler>();
 
         var app = builder.Build();
 
@@ -139,13 +216,175 @@ public class AspNetCore_Test
         await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
         Assert.IsTrue(ws.State == WebSocketState.Closed, "WebSocket is not closed.");
     }
+
+    [TestMethod]
+    public async Task TestWebServerDigestAuth()
+    {
+        var builder = WebApplication.CreateBuilder([]); ;
+
+        builder.WebHost.UseKestrel(options =>
+        {
+            options.ListenLocalhost(50000);
+        });
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddRDPGW();
+
+        builder.Services.AddSingleton<IRDPGWAuthenticationHandler, AuthHandler>();
+        builder.Services.AddSingleton<IRDPGWAuthorizationHandler, SuccessAuthorizationHandler>();
+
+        var app = builder.Build();
+
+        app.UseRDPGW();
+
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        var appTask = Task.Run(app.Run);
+
+        await Task.Delay(1000);
+
+        var client = new HttpClient();
+        var response = await client.GetAsync("http://localhost:50000/");
+
+        Assert.IsFalse(response.IsSuccessStatusCode, "Response was successful. Expected failure.");
+        Assert.AreEqual(404, (int)response.StatusCode, "Expected 404 Not Found status code.");
+
+        ClientWebSocket ws = new ClientWebSocket();
+        try
+        {
+            ws.Options.SetRequestHeader("Authorization", "Digest " + Convert.ToBase64String(Encoding.UTF8.GetBytes("user:password")));
+            await ws.ConnectAsync(new Uri("ws://localhost:50000/remoteDesktopGateway"), CancellationToken.None);
+        }
+        catch 
+        {
+            Assert.Fail("Expected WebSocket to be accepted.");
+            return;
+        }
+
+        Assert.IsTrue(ws.State == WebSocketState.Open, "WebSocket is not open.");
+
+        var packets = JsonConvert.DeserializeObject<TestPacket[]>(File.ReadAllText("packets.json"));
+        Assert.IsNotNull(packets, "Failed to deserialize packets.json");
+
+
+        var clientPackets = packets!.Where(p => p.Type == "client" && p.TypeName != "HTTP_DATA_PACKET").ToArray();
+
+        foreach(var packet in clientPackets)
+        {
+            Console.WriteLine($"Sending packet: {packet.TypeName}");
+            await ws.SendAsync(packet.Data, WebSocketMessageType.Binary, true, CancellationToken.None);
+            var buffer = new byte[10240];
+            Console.WriteLine($"Waiting for packet: {packet.Expected}");
+            var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            Assert.IsTrue(result.MessageType == WebSocketMessageType.Binary, "Expected binary message type.");
+            Assert.IsTrue(result.EndOfMessage, "Expected end of message.");
+            var data = buffer.Take(result.Count).ToArray();
+        
+            var packetMessage = HTTP_PACKET.FromBytes(data);
+            Assert.IsTrue(packetMessage.GetType().Name == packet.Expected, $"Type mismatch for packet: {packetMessage.GetType().Name} != {packet.Expected}");
+        }
+
+        await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+        Assert.IsTrue(ws.State == WebSocketState.Closed, "WebSocket is not closed.");
+    }
+
+    [TestMethod]
+    public async Task TestWebServerResourceAuthFail()
+    {
+        var builder = WebApplication.CreateBuilder([]); ;
+
+        builder.WebHost.UseKestrel(options =>
+        {
+            options.ListenLocalhost(50000);
+        });
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddRDPGW();
+
+        builder.Services.AddSingleton<IRDPGWAuthenticationHandler, AuthHandler>();
+        builder.Services.AddSingleton<IRDPGWAuthorizationHandler, FailAuthorizationHandler>();
+
+        var app = builder.Build();
+
+        app.UseRDPGW();
+
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        var appTask = Task.Run(app.Run);
+
+        await Task.Delay(1000);
+
+        var client = new HttpClient();
+        var response = await client.GetAsync("http://localhost:50000/");
+
+        Assert.IsFalse(response.IsSuccessStatusCode, "Response was successful. Expected failure.");
+        Assert.AreEqual(404, (int)response.StatusCode, "Expected 404 Not Found status code.");
+
+        ClientWebSocket ws = new ClientWebSocket();
+        try
+        {
+            ws.Options.SetRequestHeader("Authorization", "Digest " + Convert.ToBase64String(Encoding.UTF8.GetBytes("user:password")));
+            await ws.ConnectAsync(new Uri("ws://localhost:50000/remoteDesktopGateway"), CancellationToken.None);
+        }
+        catch 
+        {
+            Assert.Fail("Expected WebSocket to be accepted.");
+            return;
+        }
+
+        Assert.IsTrue(ws.State == WebSocketState.Open, "WebSocket is not open.");
+
+        var packets = JsonConvert.DeserializeObject<TestPacket[]>(File.ReadAllText("packets.json"));
+        Assert.IsNotNull(packets, "Failed to deserialize packets.json");
+
+
+        var clientPackets = packets!.Where(p => p.Type == "client" && p.TypeName != "HTTP_DATA_PACKET").ToArray();
+
+        foreach(var packet in clientPackets)
+        {
+            Console.WriteLine($"Sending packet: {packet.TypeName}");
+            await ws.SendAsync(packet.Data, WebSocketMessageType.Binary, true, CancellationToken.None);
+            var buffer = new byte[10240];
+            Console.WriteLine($"Waiting for packet: {packet.Expected}");
+            var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            Assert.IsTrue(result.MessageType == WebSocketMessageType.Binary, "Expected binary message type.");
+            Assert.IsTrue(result.EndOfMessage, "Expected end of message.");
+            var data = buffer.Take(result.Count).ToArray();
+        
+            var packetMessage = HTTP_PACKET.FromBytes(data);
+            Assert.IsTrue(packetMessage.GetType().Name == packet.Expected, $"Type mismatch for packet: {packetMessage.GetType().Name} != {packet.Expected}");
+        }
+
+        await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+        Assert.IsTrue(ws.State == WebSocketState.Closed, "WebSocket is not closed.");
+    }
 }
 
-public class AuthorizationHandler : IRDPGWAuthorizationHandler
+public class SuccessAuthorizationHandler : IRDPGWAuthorizationHandler
 {
     public Task<bool> HandleUserAuthorization(string userId, string resource)
     {
         return Task.FromResult(true);
+    }
+}
+
+public class FailAuthorizationHandler : IRDPGWAuthorizationHandler
+{
+    public Task<bool> HandleUserAuthorization(string userId, string resource)
+    {
+        return Task.FromResult(false);
     }
 }
 
